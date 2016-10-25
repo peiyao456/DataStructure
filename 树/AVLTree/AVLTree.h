c5f86c4e347ff4e3c9b92279c38ca5a561eca74e
w <1521107105@qq.com>
2016-10-25 21:02:42 +0800

    AVL树

--- 树/AVLTree/AVLTree.h
+++ 树/AVLTree/AVLTree.h
@@ -60,48 +60,56 @@ public:
 			}
 			//无法插入
 			else
-			{
 				return false;
-			}
 		}
-		Node* newNode = new Node(key);
+		 cur = new Node(key);
+		 cur->_parent = parent;
 		if (parent->_key < key)
 		{
-			parent->_right = newNode;
+			parent->_right = cur;
 		}
 		else
 		{
-			parent->_left = newNode;
+			parent->_left = cur;
 		}
-		newNode->_parent = parent;
 
 		while (parent)//调整到根节点之后就不再调整
 		{
+			//if (parent->_parent && parent->_parent->_left == parent)
+			if(cur == parent->_left)
+				--parent->_bf;
+			else if (cur == parent->_right)             //if (parent->_parent->_right == parent)
+				++parent->_bf;
+
 			if (parent->_bf == 0)//说明parent的高度并没有改变，此时调整结束
-				break;
-			if (parent->_parent->_left == parent)
-				parent->_bf--;
-			else                 //if (parent->_parent->_right == parent)
-				parent->_bf++;
-			
-			if (parent->_bf == 2)//树已经不平衡，需要调整
+				return true;
+			else if (parent->_bf == 1 || parent->_bf == -1)
 			{
-				if (parent->_right->_bf == 1)
-					_RotateL(parent);
-				else
-					_RotateRL(parent);
+				cur = parent;
+				parent = parent->_parent;
 			}
-			if (parent->_bf == -2)
+			else
 			{
-				if (parent->_right->_bf == -1)
-					_RotateR(parent);
+				if (parent->_bf == 2)//树已经不平衡，需要调整
+				{
+					if (cur->_bf == 1)
+						_RotateL(parent);
+					else             //if (cur->_bf == -1)
+						_RotateRL(parent);
+				}
+			//	if (parent->_bf == -2)
 				else
-					_RotateLR(parent);
+				{
+					if (cur->_bf == -1)
+						_RotateR(parent);
+					else                 //if (cur->_bf == 1)
+						_RotateLR(parent);
+				}
 			}
-			parent = parent->_parent;
 		}
 		return true;
 	}
+	
 	void InOrderNonR()
 	{
 		if (_root == NULL)
@@ -122,7 +130,62 @@ public:
 		}
 		cout << endl;
 	}
+	bool IsBalance()
+	{
+		return _IsBalance(_root);
+	}
+	bool IsBalanceOP()
+	{
+		int height = 0;
+		return _IsBalanceOP(_root,height);
+	}
+	size_t Height()
+	{
+		return _Height(_root);
+	}
 protected:
+	//优化版本
+	bool _IsBalanceOP(Node* root,int& height)
+	{
+		if (root == NULL)
+		{
+			height = 0;
+			return true;
+		}
+		int leftHeight = 0;
+		if (!_IsBalanceOP(root->_left, leftHeight))
+			return false;
+		int rightHeight = 0;
+		if (!_IsBalanceOP(root->_right, leftHeight))
+			return false;
+		height = 1 + rightHeight > leftHeight ? rightHeight : leftHeight;
+		return true;
+	}
+	size_t _Height(Node* root)
+	{
+		if (root == NULL)
+			return 0;
+		int leftHeight = _Height(root->_left);
+		int rightHeight = _Height(root->_right);
+		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+	}
+	//大多数结点会计算两次，时间复杂度0(N*N)
+	bool _IsBalance(Node* root)
+	{
+		if (root == NULL)
+			return true;
+		int leftHeight = _Height(root->_left);
+		int rightHeight = _Height(root->_right);
+
+		if (rightHeight - leftHeight != root->_bf)
+		{
+			cout << "平衡因子异常" << root->_key << " ";
+		}
+
+		return abs(rightHeight - leftHeight) < 2
+			&& _IsBalance(root->_left)
+			&& _IsBalance(root->_right);
+	}
 	void _RotateR(Node* parent)
 	{
 		Node* subL = parent->_left;
@@ -188,13 +251,55 @@ protected:
 	}
 	void _RotateLR(Node* parent)
 	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+		int bf = subLR->_bf;
 		_RotateL(parent->_left);
 		_RotateR(parent);
+		if (bf == 0)
+		{
+			parent->_bf = 0;
+			subL->_bf = 0;
+			subLR->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			parent->_bf = 0;
+			subL->_bf = -1;
+			subLR->_bf = 1;
+		}
+		else // bf == -1
+		{
+			parent->_bf = 1;
+			subL->_bf = 0;
+			subLR->_bf = -1;
+		}
 	}
 	void _RotateRL(Node* parent)
 	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		int bf = subRL->_bf;
 		_RotateR(parent->_right);
 		_RotateL(parent);
+		if (bf == 0)
+		{
+			parent->_bf = 0;
+			subR->_bf = 0;
+		}
+		else if (bf == 1)
+		{
+			parent->_bf = -1;
+			subR->_bf = 0;
+			subRL->_bf = 1;
+		}
+		else
+		{
+			parent->_bf = 0;
+			subR->_bf = 1;
+			subRL->_bf = -1;
+		}
+
 	}
 	void _Destroy(Node* root)
 	{
@@ -209,11 +314,23 @@ private:
 };
 void TestAVL()
 {
-	AVLTree<int, int> at;
-	int array[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
-	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+	AVLTree<int, int> tree1;
+	int array1[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	for (int i = 0; i < sizeof(array1) / sizeof(array1[0]); ++i)
 	{
-		at.InsertNonR(array[i]);
+		tree1.InsertNonR(array1[i]);
 	}
-	at.InOrderNonR();
+	tree1.InOrderNonR();
+	cout <<"IsBalance?"<< tree1.IsBalance() << endl;
+	cout << "IsBalance?" << tree1.IsBalanceOP() << endl;
+	int array2[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+
+	AVLTree<int, int> tree2;
+	for (size_t i = 0;i<sizeof(array2) / sizeof(array2[0]);++i)
+	{
+		tree2.InsertNonR(array2[i]);
+	}
+	tree2.InOrderNonR();
+	cout << "IsBalance?" << tree2.IsBalance() << endl;
+	cout << "IsBalance?" << tree2.IsBalanceOP() << endl;
 }
