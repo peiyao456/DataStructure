6bf480e9338e550ba38d4f6d353a5a0966be7050
w <1521107105@qq.com>
2016-11-06 20:31:29 +0800

    哈希表

--- 树/HashTable/HashTable.hpp
+++ 树/HashTable/HashTable.hpp
@@ -1,3 +1,5 @@
+
+
 #pragma once
 #include<iostream>
 using namespace std;
@@ -15,17 +17,17 @@ namespace Open
 	};
 
 
-	template<typename K,typename V>
+	template<typename K, typename V>
 	struct KVNode
 	{
 		K _key;
 		V _value;
 
 		Status _status;//保存某个位置的状态
-		KVNode(const K& key = K(),const V& value = V())
+		KVNode(const K& key = K(), const V& value = V())
 			:_key(key)
-			,_value(value)
-			,_status(EMPTY)
+			, _value(value)
+			, _status(EMPTY)
 		{}
 	};
 	template<typename K>
@@ -43,7 +45,7 @@ namespace Open
 		{
 			unsigned int seed = 131;// 31 131 1313 13131 131313
 			unsigned int hash = 0;
-			while(*str)
+			while (*str)
 			{
 				hash = hash * seed + (*str++);
 			}
@@ -55,10 +57,10 @@ namespace Open
 		}
 	};
 
-	template<typename K,typename V,typename GetK = __GetK<K>>
+	template<typename K, typename V, typename GetK = __GetK<K>>
 	class HashTable
 	{
-		typedef KVNode<K,V> Node;
+		typedef KVNode<K, V> Node;
 
 	public:
 		HashTable()
@@ -71,27 +73,27 @@ namespace Open
 		{}
 
 
-		void Swap(HashTable<K,V,GetK> ht)
+		void Swap(HashTable<K, V, GetK>& ht)
 		{
-			swap(_size,ht._size);
-			swap(_tables,ht._tables);
+			swap(_size, ht._size);
+			_tables.swap(ht._tables);
 		}
 
-		bool Insert(const K& key,const V& value)
+		bool Insert(const K& key, const V& value)
 		{
 			_CheckCapacity();
-			int index = _GetIndex(key,value);
-			
-			while(_tables[index]._status == EXIST)
+			int index = _GetIndex(key, value);
+
+			while (_tables[index]._status == EXIST)
 			{
-				if(_tables[index]._key == key)//要插入的值在原表中已经存在
+				if (_tables[index]._key == key)//要插入的值在原表中已经存在
 				{
 					return false;
 				}
 
 				++index;
 
-				if(index == _tables.size())
+				if (index == _tables.size())
 				{
 					index = 0;
 				}
@@ -104,24 +106,24 @@ namespace Open
 			++_size;
 		}
 
-		Node* Find(const K& key,const V& value)
+		Node* Find(const K& key, const V& value)
 		{
-			int index = _GetIndex(key,value);
+			int index = _GetIndex(key, value);
 			int begin = index;
-			while(_tables[index]._key != key)
+			while (_tables[index]._key != key)
 			{
 				++index;
-				if(index == _tables.size())
+				if (index == _tables.size())
 				{
 					index = 0;
 				}
-				if(index == begin)
+				if (index == begin)
 				{
 					return NULL;
 				}
 			}
 			//有可能是要删除的已经存在的元素
-			if(_tables[index]._status == EXIST)
+			if (_tables[index]._status == EXIST)
 			{
 				return &_tables[index];
 			}
@@ -129,15 +131,15 @@ namespace Open
 				return NULL;
 		}
 
-		bool Remove(const K& key,const V& value)
+		bool Remove(const K& key, const V& value)
 		{
-			if(_size == 0)
+			if (_size == 0)
 				return false;
-			int index = _GetIndex(key,value);
+			int index = _GetIndex(key, value);
 			int begin = index;
-			while(_tables[index]._status != EMPTY)
+			while (_tables[index]._status != EMPTY)
 			{
-				if(_tables[index]._key == key && _tables[index]._status == EXIST)
+				if (_tables[index]._key == key && _tables[index]._status == EXIST)
 				{
 					_tables[index]._status = DELETE;
 					--_size;
@@ -145,11 +147,11 @@ namespace Open
 				}
 
 				++index;
-				if(index == _tables.size())
+				if (index == _tables.size())
 				{
 					index = 0;
 				}
-				if(index == begin)//已经遍历一圈
+				if (index == begin)//已经遍历一圈
 				{
 					return false;
 				}
@@ -158,16 +160,16 @@ namespace Open
 	protected:
 		void _CheckCapacity()
 		{
-			if(_size*10 / _tables.size() >= 8)//保证查找效率
+			if (_size * 10 / _tables.size() >= 8)//保证查找效率
 			{
 				int newSize = _GetNewSize(_tables.size());
-				HashTable<K,V,GetK> hash;
+				HashTable<K, V, GetK> hash;
 				hash._tables.resize(newSize);
-				for(size_t i = 0; i < _size; ++i)
+				for (size_t i = 0; i < _size; ++i)
 				{
-					if(_tables[i]._status == EXIST)
+					if (_tables[i]._status == EXIST)
 					{
-						hash.Insert(_tables[i]._key,_tables[i]._value);
+						hash.Insert(_tables[i]._key, _tables[i]._value);
 					}
 				}
 				this->Swap(hash);
@@ -176,27 +178,28 @@ namespace Open
 				return;
 		}
 
-		int _GetIndex(const K& key,const V& value)
+		int _GetIndex(const K& key, const V& value)
 		{
 			GetK getK;
 			return getK(key) % _tables.size();
 		}
 		int _GetNewSize(int num)
 		{
-			const int _PrimeSize= 28;
+			const int _PrimeSize = 28;
 			static const unsigned long _PrimeList[_PrimeSize] = {
-			53ul, 97ul, 193ul, 389ul, 769ul,
-			1543ul, 3079ul, 6151ul, 12289ul, 24593ul,
-			49157ul, 98317ul, 196613ul, 393241ul,786433ul,
-			1572869ul, 3145739ul, 6291469ul, 12582917ul,
-			25165843ul,50331653ul, 100663319ul, 201326611ul, 402653189ul,
-			805306457ul,1610612741ul, 3221225473ul, 4294967291ul
+				53ul, 97ul, 193ul, 389ul, 769ul,
+				1543ul, 3079ul, 6151ul, 12289ul, 24593ul,
+				49157ul, 98317ul, 196613ul, 393241ul, 786433ul,
+				1572869ul, 3145739ul, 6291469ul, 12582917ul,
+				25165843ul, 50331653ul, 100663319ul, 201326611ul, 402653189ul,
+				805306457ul, 1610612741ul, 3221225473ul, 4294967291ul
 			};
-			for(int i = 0; i < _PrimeSize; ++i)
+			for (int i = 0; i < _PrimeSize; ++i)
 			{
-				if(_PrimeList[i] > num)
+				if (_PrimeList[i] > num)
 					return _PrimeList[i];
 			}
+			return _PrimeList[_PrimeSize - 1];
 		}
 	protected:
 		vector<Node> _tables;
@@ -207,27 +210,27 @@ namespace Open
 
 void TestHashTableOpen()
 {
-	Open::HashTable<int,int>  ht1;//模板参数3是采用缺省的参数
-	int array1[] = {89,18,8,58,2,3,4,9,0};
-	for(int i = 0; i < sizeof(array1)/sizeof(array1[0]); ++i)
+	Open::HashTable<int, int>  ht1;//模板参数3是采用缺省的参数
+	int array1[] = { 89, 18, 8, 58, 2, 3, 4, 9, 0 };
+	for (int i = 0; i < sizeof(array1) / sizeof(array1[0]); ++i)
 	{
-		ht1.Insert(array1[i],0);
+		ht1.Insert(array1[i], 0);
 	}
-	ht1.Remove(8,0);
-	ht1.Remove(1,0);
-	
-	Open::HashTable<string,int,Open::__GetStrK> ht2;
-	char* array2[] = {"hello","world","sort","find","sort"};
-	for(int i = 0; i < sizeof(array2)/sizeof(array2[0]); ++i)
+	ht1.Remove(8, 0);
+	ht1.Remove(1, 0);
+
+	Open::HashTable<string, int, Open::__GetStrK> ht2;
+	char* array2[] = { "hello", "world", "sort", "find", "sort" };
+	for (int i = 0; i < sizeof(array2) / sizeof(array2[0]); ++i)
 	{
-		Open::KVNode<string,int>* node = ht2.Find(array2[i],0);
-		if(node)//结点已经存在
+		Open::KVNode<string, int>* node = ht2.Find(array2[i], 0);
+		if (node)//结点已经存在
 		{
 			node->_value++;
 		}
 		else
 		{
-			ht2.Insert(array2[i],0);
+			ht2.Insert(array2[i], 0);
 		}
 	}
 }
@@ -236,17 +239,17 @@ void TestHashTableOpen()
 //开链法
 namespace Link
 {
-	template<typename K,typename V>
+	template<typename K, typename V>
 	struct KVNode
 	{
 		K _key;
 		V _value;
-		KVNode<K,V>* _next;
+		KVNode<K, V>* _next;
 
-		KVNode(const K& key = K(),const V& value = V())
-		:_key(key)
-		,_value(value)
-		,_next(NULL)
+		KVNode(const K& key = K(), const V& value = V())
+			:_key(key)
+			, _value(value)
+			, _next(NULL)
 		{}
 	};
 
@@ -264,9 +267,9 @@ namespace Link
 	{
 		static size_t BKDRHash(const char* str)
 		{
-			unsigned int seed = 131;// 31 131 1313 13131 131313
+			unsigned int seed = 131;
 			unsigned int hash = 0;
-			while(*str)
+			while (*str)
 			{
 				hash = hash * seed + (*str++);
 			}
@@ -278,10 +281,10 @@ namespace Link
 		}
 	};
 
-	template<typename K,typename V,typename GetK = __GetK<K>>
+	template<typename K, typename V, typename GetK = __GetK<K>>
 	class HashTable
 	{
-		typedef KVNode<K,V> Node;
+		typedef KVNode<K, V> Node;
 	public:
 		HashTable()
 			:_size(0)
@@ -289,20 +292,40 @@ namespace Link
 			_tables.resize(2);//初始化开辟2个空间
 		}
 
-		bool Insert(const K& key,const V& value)
+		~HashTable()
+		{
+			Node* cur = NULL;
+			Node* del = NULL;
+		
+			for (int i = 0; i <(int)_tables.size(); ++i)
+			{
+				cur = _tables[i];
+				while (cur)
+				{
+					del = cur;
+					cur = cur->_next;
+					delete del;
+					del = NULL;
+				}
+				
+			}
+			_tables.clear();
+			_size = 0;
+		}
+		bool Insert(const K& key, const V& value)
 		{
 			//检查容量
 			_CheckCapacity();
-			int index = _GetIndex(key);
+			int index = _GetIndex(key, _tables.size());
 			//先查找，看要插入的元素是否已经存在
-			if(Find(key))
+			if (Find(key))
 			{
 				return false;
 			}
 			//插入元素分两种情况
 			//1.插入的结点是第一个结点
 			//2.插入的结点不是第一个结点
-			Node* newNode = new Node(key,value);
+			Node* newNode = new Node(key, value);
 			newNode->_next = _tables[index];
 			_tables[index] = newNode;
 			++_size;
@@ -310,11 +333,15 @@ namespace Link
 
 		Node* Find(const K& key)
 		{
-			int index = _GetIndex(key);
-			Node* cur = _tables[index];
-			while(cur)
+			if (_size == 0)
 			{
-				if(cur->_key == key)
+				return false;
+			}
+			int index = _GetIndex(key, _tables.size());
+			Node* cur = _tables[index];
+			while (cur)
+			{
+				if (cur->_key == key)
 					return cur;
 				cur = cur->_next;
 			}
@@ -323,7 +350,11 @@ namespace Link
 
 		bool Remove(const K& key)
 		{
-			int index = _GetIndex(key);
+			if (_size == 0)
+			{
+				return false;
+			}
+			int index = _GetIndex(key, _tables.size());
 			//删除分3种情况
 			//1.删除第一个节点
 			//2.删除中间结点
@@ -331,11 +362,11 @@ namespace Link
 			Node* cur = _tables[index];
 			//记录要删除结点的上一个结点
 			Node* prev = NULL;
-			while(cur)
+			while (cur)
 			{
-				if(cur->_key == key)//找到要删除的结点
+				if (cur->_key == key)//找到要删除的结点
 				{
-					if(prev == NULL)//要删除的就是第一个结点
+					if (prev == NULL)//要删除的就是第一个结点
 					{
 						_tables[index] = cur->_next;
 					}
@@ -353,109 +384,165 @@ namespace Link
 			return false;
 		}
 
-	protected:
-		int _GetIndex(const K& key)
+		void Print()
 		{
-			GetK getK;
-			return getK(key) % _tables.size();
-		}
-
-		void _CheckCapacity()
-		{
-			if(_size == _tables.size())
+			if (_size == 0)
+				return;
+			for (size_t i = 0; i < _tables.size(); ++i)
 			{
-				int newSize = _GetNewSize(_size);//容量扩大
-				HashTable<K,V,GetK> tmp;
-				tmp._tables.resize(newSize);
-				vector<K> v;
-				Node* del = NULL;
-
-				//把所有元素放进vector中
-				for(size_t i = 0; i < _size; ++i)
+				Node* cur = _tables[i];
+				cout << "_tables[" << i << "]" << "->";
+				while (cur)
 				{
-					//找到有存储元素的链
-					if(_tables[i] != NULL)
-					{
-						Node* cur = _tables[i];
-						while(cur)
-						{
-						    //cur = _tables[i];
-							v.push_back(cur->_key);
-							cur = cur->_next;
-						}
-
-						//清理空间
-						cur = _tables[i];
-						while(cur)
-						{
-							del = cur;
-							cur = cur->_next;
-							delete del;
-						}
-					}
-					else
-						continue;
+					cout << cur->_key << "->";
+					cur = cur->_next;
 				}
-				//将vector中的所有元素重新插入
-				for(size_t i = 0 ; i < v.size(); ++i)
-				{
-					tmp.Insert(v[i],0);
-					//v.pop_back();
-				}
-				//已经移动完成
-				this->_Swap(tmp);
+				cout << "NULL" << endl;
 			}
 		}
-		
+	protected:
+		int _GetIndex(const K& key, size_t size)
+		{
+			GetK getK;
+			return getK(key) % size;
+		}
+
+
+		//增容过程中，多一次释放，多一次开辟，导致空间 利用率不是很高
+		//void _CheckCapacity()
+		//{
+		//	if(_size == _tables.size())
+		//	{
+		//		int newSize = _GetNewSize(_size);//容量扩大
+		//		HashTable<K,V,GetK> tmp;
+		//		tmp._tables.resize(newSize);
+		//		vector<K> v;
+		//		Node* del = NULL;
+
+		//		//把所有元素放进vector中
+		//		for(size_t i = 0; i < _size; ++i)
+		//		{
+		//			//找到有存储元素的链
+		//			if(_tables[i] != NULL)
+		//			{
+		//				Node* cur = _tables[i];
+		//				while(cur)
+		//				{
+		//				    //cur = _tables[i];
+		//					v.push_back(cur->_key);
+		//					cur = cur->_next;
+		//				}
+
+		//				//清理空间
+		//				cur = _tables[i];
+		//				while(cur)
+		//				{
+		//					del = cur;
+		//					cur = cur->_next;
+		//					delete del;
+		//				}
+		//			}
+		//			else
+		//				continue;
+		//		}
+		//		//将vector中的所有元素重新插入
+		//		for(size_t i = 0 ; i < v.size(); ++i)
+		//		{
+		//			tmp.Insert(v[i],0);
+		//			//v.pop_back();
+		//		}
+		//		//已经移动完成
+		//		this->_Swap(tmp);
+		//	}
+		//}
+
+
+		//直接将原来表中的结点连接到新的表中
+		void _CheckCapacity()
+		{
+			if (_size == _tables.size())
+			{
+				int newSize = _GetNewSize(_size);//容量扩大
+				HashTable<K, V, GetK> tmp;
+				tmp._tables.resize(newSize);
+				for (size_t i = 0; i < _size; ++i)
+				{
+					Node* cur = _tables[i];
+					Node* next = NULL;
+					while (cur)
+					{
+						next = cur->_next;
+						//获得该节点在新的表中的下标
+						int index = _GetIndex(cur->_key, newSize);
+
+						//将对应的结点插入到新的表中
+						//头插法
+						//两种情况，可以一起处理 
+						cur->_next = tmp._tables[index];
+						tmp._tables[index] = cur;
+
+						cur = next;
+					}
+					_tables[i] = NULL;//这里必须置空，不要让两个空间同时指向一块内存
+				}
+				_Swap(tmp);
+			}
+		}
+
 		int _GetNewSize(int num)
 		{
-			const int _PrimeSize= 28;
+			const int _PrimeSize = 28;
 			static const unsigned long _PrimeList[_PrimeSize] = {
-			53ul, 97ul, 193ul, 389ul, 769ul,
-			1543ul, 3079ul, 6151ul, 12289ul, 24593ul,
-			49157ul, 98317ul, 196613ul, 393241ul,786433ul,
-			1572869ul, 3145739ul, 6291469ul, 12582917ul,
-			25165843ul,50331653ul, 100663319ul, 201326611ul, 402653189ul,
-			805306457ul,1610612741ul, 3221225473ul, 4294967291ul
+				53ul, 97ul, 193ul, 389ul, 769ul,
+				1543ul, 3079ul, 6151ul, 12289ul, 24593ul,
+				49157ul, 98317ul, 196613ul, 393241ul, 786433ul,
+				1572869ul, 3145739ul, 6291469ul, 12582917ul,
+				25165843ul, 50331653ul, 100663319ul, 201326611ul, 402653189ul,
+				805306457ul, 1610612741ul, 3221225473ul, 4294967291ul
 			};
-			for(int i = 0; i < _PrimeSize; ++i)
+			for (int i = 0; i < _PrimeSize; ++i)
 			{
-				if(_PrimeList[i] > num)
+				if (_PrimeList[i] > num)
 					return _PrimeList[i];
 			}
 		}
 
-		void _Swap(HashTable<K,V,GetK> ht)
+		void _Swap(HashTable<K, V, GetK>& ht)
 		{
-			swap(ht._tables,_tables);
-			swap(ht._size,_size);
+			//std::swap(ht._tables,_tables);
+			_tables.swap(ht._tables);
+			swap(ht._size, _size);
 		}
 
 	protected:
 		vector<Node* > _tables;
 		size_t _size;
 	};
-	
 }
 void TestHashTableLink()
 {
-	Link::HashTable<int,int>  ht1;
-	int array1[] = {89,21,8,58,53,12,3,4,9,0};
-	for(int i = 0; i < sizeof(array1)/sizeof(array1[0]); ++i)
+	Link::HashTable<int, int>  ht1;
+	int array1[] = { 89, 21, 8, 58, 53, 12, 3, 4, 9, 0 };
+	for (int i = 0; i < sizeof(array1) / sizeof(array1[0]); ++i)
 	{
-		ht1.Insert(array1[i],0);
+		ht1.Insert(array1[i], 0);
 	}
+	ht1.Print();
 	ht1.Remove(4);
 	ht1.Remove(12);
-	
+	ht1.Print();
+
+
 	Link::HashTable<string,int> ht2;
 	char* array2[] = {"hello","yang","hello","sort","wang","zip","huffman"};
 	for(int i = 0; i < sizeof(array2)/sizeof(array2[0]); ++i)
 	{
-		Link::KVNode<string,int>* ret = ht2.Find(array2[i]);
-		if(!ret)
-			ht2.Insert(array2[i],0);
+	Link::KVNode<string,int>* ret = ht2.Find(array2[i]);
+	if(!ret)
+	ht2.Insert(array2[i],0);
 	}
 	ht2.Remove("hello");
 	ht2.Remove("sort");
+
+	ht2.Print();
 }
